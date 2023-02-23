c2c54847541c45c5ac056f10dff5b6e0d459b851
55973 <55973@etu.he2b.be>
2023-02-23 22:01:59 +0100

    v

--- projetbis/src/components/choix/choix.js
+++ projetbis/src/components/choix/choix.js
@@ -9,8 +9,8 @@ export const Choix = () => {
 
     const [acceuil, setAcceuil]
         = useState('block')
-    const [rebours, setRebours] = useState(3)
-    const [opa, setOpa] = useState('opacity-1')
+
+    const [opa, setOpa] = useState('opacity-0')
     const [solid, setSolid] = useState('')
     const [partie, setPartie] = useState('hidden')
 
@@ -18,7 +18,7 @@ export const Choix = () => {
     const [imgPerso, setImgperso] = useState()
     const [choix, setChoix] = useState('')
 
-
+    const [rebours, setRebours] = useState(3);
     const trois = ["yellow", "blue", "red"]
     const [choixAdv, setChoixadv] = useState(trois[Math.floor(Math.random() * trois.length)])
     const [imgAdv, setImgAdv] = useState()
@@ -31,40 +31,18 @@ export const Choix = () => {
 
 
 
-    // effect
 
-
-
-    const e = useEffect
-    e(() => {
-        let cmpt = setInterval(() => {
-
-            setRebours(rebours => rebours - 1)
-            if (rebours <= 0) {
-                setOpa("opacity-0")
-                setSolid('border-[22px] ')
-                setRebours(0)
-            }
-            else {
-
-                setOpa("opacity-1")
-                setSolid('')
-
-            }
-
-        }, 1000)
-        return () => {
-            clearInterval(cmpt)
-        }
-
-    })
-
-
+    const border = trois[Math.floor(Math.random() * trois.length)];
 
 
     //Function pour le score et random
     const random = () => {
-        const border = trois[Math.floor(Math.random() * trois.length)];
+
+
+
+
+
+
         setChoixadv(border);
 
         setImgAdv(() => {
@@ -79,55 +57,101 @@ export const Choix = () => {
         });
 
 
-       setTimeout(() => {
-        setWin(() => {
-            if (choix === "red" && choixAdv === "red") {
-                return 'Egalité red'
-            }
-            else if (choix === "yellow" && choixAdv === "yellow") {
-                return 'Egalité yellow'
-            }
-            else if (choix === "blue" && choixAdv === "blue") {
-                return 'Egalité blue'
-            }
 
 
-            else if (choix === "yellow" && choixAdv === "blue") {
-                setScore(score + 1)
-                return 'you win'
-            }
-            else if (choix === "yellow" && choixAdv === "red") {
-                return 'you lose'
-            }
-
-            else if (choix === "blue" && choixAdv === "yellow") {
-                return 'you lose'
-            }
-            else if (choix === "blue" && choixAdv === "red") {
-                setScore(score + 1)
-                return 'you win'
-            }
-            else if (choix === "red" && choixAdv === "yellow") {
-                setScore(score + 1)
-                return 'you win'
-            }
-            else if (choix === "red" && choixAdv === "blue") {
-                return 'you lose'
-            }
-
-        })
-       }, 6000);
-
-
-
-        setRebours(4)
         setTimeout(() => {
             setPartie('block')
             setAcceuil('hidden')
         }, 1000);
 
+        // Counter()
+
 
     }
+
+    const compare = (choix, choix2) => {
+        setTimeout(() => {
+            if (choix === "red" && choix2 === "red") {
+                setWin('Egalité red');
+            }
+            else if (choix === "yellow" && choix2 === "yellow") {
+                setWin('Egalité yellow');
+            }
+            else if (choix === "blue" && choix2 === "blue") {
+                setWin('Egalité blue');
+            }
+            else if (choix === "yellow" && choix2 === "blue") {
+                setScore(score + 1);
+                setWin('you win');
+            }
+            else if (choix === "yellow" && choix2 === "red") {
+                setScore(score - 1);
+                setWin('you lose');
+            }
+            else if (choix === "blue" && choix2 === "yellow") {
+                setScore(score - 1);
+                setWin('you lose');
+            }
+            else if (choix === "blue" && choix2 === "red") {
+                setScore(score + 1);
+                setWin('you win');
+            }
+            else if (choix === "red" && choix2 === "yellow") {
+                setScore(score + 1);
+                setWin('you win');
+            }
+            else if (choix === "red" && choix2 === "blue") {
+                setScore(score - 1);
+                setWin('you lose');
+            }
+            else {
+
+            }
+        }, 6000);
+    }
+
+    // effect
+
+
+
+    // useEffect(() => {
+    // },[rebours])
+    // const [rebours, setRebours] = useState(3);
+    // const [isHidden, setIsHidden] = useState(false);
+
+    // const Counter = () => {
+
+
+    //     useEffect(() => {
+    //         const intervalId = setInterval(() => {
+    //             setRebours(rebours => rebours - 1);
+    //         }, 1000);
+
+    //         return () => {
+    //             clearInterval(intervalId);
+    //         };
+    //     }, []);
+
+    //     useEffect(() => {
+    //         if (rebours <= 0) {
+    //             setIsHidden(true);
+    //             setRebours(3);
+    //         } else {
+    //             setIsHidden(false);
+    //         }
+    //     }, [rebours]);
+
+    //     const rebours = isHidden ? "opacity-0 border-[22px]" : "opacity-1";
+
+
+    // }
+
+
+
+
+    useEffect(() => {
+        compare(choix, choixAdv);
+    }, [choix, choixAdv]);
 
 
 
@@ -138,6 +162,9 @@ export const Choix = () => {
         setImgperso(<img src={papier} alt="" />)
         random()
 
+        console.log('mon choix es ' + choix);
+        console.log('son choix es ' + choixAdv);
+
 
     }
 
@@ -145,14 +172,16 @@ export const Choix = () => {
         setChoix('yellow')
         setImgperso(<img src={ciseaux} alt="" />)
         random()
-
+        console.log('mon choix es ' + choix);
+        console.log('son choix es ' + choixAdv);
     }
 
     const rock = () => {
         setChoix('red')
         setImgperso(<img src={pierre} alt="" />)
         random()
-
+        console.log('mon choix es ' + choix);
+        console.log('son choix es ' + choixAdv);
     }
 
     const rejouer = () => {
@@ -162,6 +191,7 @@ export const Choix = () => {
         }, 300);
 
     }
+
 
 
 
@@ -177,11 +207,11 @@ export const Choix = () => {
                 <div className='flex justify-center mt-[100px]'>
                     <div className='w-[200px] relative h-[275px]' id='bg'>
                         <div onClick={paper} className='absolute cursor-pointer -top-10 -left-10
-                     bg-white p-5 rounded-full border-[15px] border-blue-500 '><img src={papier} alt="" /></div>
+                     bg-white p-5 rounded-full border-[15px] border-blue-500 hover:translate-x-6 hover:scale-[1.5] hover:rotate-[360deg] hover:z-50 transition duration-300 ease-in-out'><img src={papier} alt="" /></div>
                         <div onClick={scissors} className='absolute cursor-pointer -top-10 -right-10
-                       bg-white p-5 rounded-full border-[15px] border-yellow-500'><img src={ciseaux} alt="" /></div>
+                       bg-white p-5 rounded-full border-[15px] border-yellow-500 hover:-translate-x-6 hover:scale-[1.5] hover:-rotate-[360deg] hover:z-50 transition duration-300 ease-in-out'><img src={ciseaux} alt="" /></div>
                         <div onClick={rock} className='absolute cursor-pointer bottom-10 left-[20%]
-                      bg-white p-5 rounded-full border-[15px] border-red-500'><img src={pierre} alt="" /></div>
+                      bg-white p-5 rounded-full border-[15px] border-red-500 hover:-translate-y-6 hover:scale-[1.5] hover:-rotate-[360deg] hover:z-50 transition duration-300 ease-in-out'><img src={pierre} alt="" /></div>
 
                     </div>
 
@@ -192,7 +222,7 @@ export const Choix = () => {
                 </div>
             </div>
 
-            <Partie partie={partie} imgPerso={imgPerso} borderChoix={choix} choixAdv={choixAdv} imgAdv={imgAdv} win={win} opa={opa} solid={solid} rebours={rebours} rejouer={rejouer} />
+            <Partie partie={partie} imgPerso={imgPerso} borderChoix={choix} choixAdv={choixAdv} imgAdv={imgAdv} win={win} opa={opa} solid={solid} rebours={rebours} rejouer={rejouer} score={score} />
 
         </div>
 
--- projetbis/src/components/partie/partie.js
+++ projetbis/src/components/partie/partie.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import { Score } from '../score/score'
 
-
 export const Partie = (props) => {
-   
-   const [solid, setSolid] = useState('')   
+
+    const [solid, setSolid] = useState('')
 
 
 
@@ -14,49 +13,58 @@ export const Partie = (props) => {
     return (
         <div className={`w-[1000px] h-[600px] bg-blue-900 pt-10 ${props.partie} `}>
 
-            <Score />
+            <Score score={props.score} />
+        
 
-            <div  className='flex justify-center mt-16 items-center text-center'>
-                <div>
-                    <div className='text-white mb-10 tracking-widest font-semibold'>YOU PICKED</div>
-                    <div className={` w-[200px] h-[200px] flex justify-center items-center 
-                     bg-white  rounded-full border-[22px] border-${props.borderChoix}-500`}> {props.imgPerso}  
-                     </div>
+
+            
+                {/* <div className='flex justify-center'>
+                    <div className='border-2 border-white flex justify-between w-[500px] p-3 rounded-xl'>
+                        <div>
+                            <img src={img1} alt="" />
+                        </div>
+                        <div className='bg-white px-7 rounded-xl text-blue-900 text-center py-3'>
+                            <p className='text-sm'>Score</p>
+                          
+                            <p className='font-bold text-5xl'>{score}</p>
+                        </div>
+                    </div>
+                </div> */}
+
+                <div className='flex justify-center mt-16 items-center text-center'>
+                    <div>
+                        <div className='text-white mb-10 tracking-widest font-semibold'>YOU PICKED</div>
+                        <div className={` w-[200px] h-[200px] flex justify-center items-center 
+                     bg-white  rounded-full border-[22px] border-${props.borderChoix}-500`}> {props.imgPerso}
+                        </div>
+                    </div>
+                    <div className='mx-10'>
+                        <p className='uppercase text-white text-3xl font-bold'>{props.win}</p>
+                        <p><button onClick={props.rejouer} className='bg-white px-10 uppercase text-sm py-2 rounded-md mt-4'>Play again</button></p>
+                    </div>
+
+
+
+                    <div>
+                        <div className='text-white mb-10 tracking-widest font-semibold'>THE HOUSE PICKED</div>
+                        <div className={`relative w-[200px] h-[200px] flex justify-center items-center 
+                     bg-white  rounded-full ${props.solid} border-${props.choixAdv}-500`}>{props.imgAdv}
+
+                            <div className={`w-full h-full bg-violet-800 rounded-full absolute flex justify-center items-center text-[100px] ${props.opa}`}>{props.rebours}</div>
+
+                        </div>
+                    </div>
+
+
                 </div>
-                <div className='mx-10'>
-                    <p className='uppercase text-white text-3xl font-bold'>{props.win}</p>
-                    <p><button onClick={props.rejouer} className='bg-white px-10 uppercase text-sm py-2 rounded-md mt-4'>Play again</button></p>
+
+
+                <div className='flex justify-end mt-12 px-5'>
+                    <div className='uppercase text-white border-2 px-9 hover:bg-white hover:text-blue-900 rounded-md py-1 border-white'>rulers</div>
                 </div>
-
-
-
-                <div>
-                <div className='text-white mb-10 tracking-widest font-semibold'>THE HOUSE PICKED</div>
-                <div className={`relative w-[200px] h-[200px] flex justify-center items-center 
-                     bg-white  rounded-full ${props.solid} border-${props.choixAdv}-500`}>{props.imgAdv} 
-
-                     <div className={`w-full h-full bg-violet-800 rounded-full absolute flex justify-center items-center text-[100px] ${props.opa}`}>{props.rebours}</div>
-
-                     </div>
-                </div>
-
-
             </div>
-
-
-            <div className='flex justify-end mt-12 px-5'>
-                <div className='uppercase text-white border-2 px-9 hover:bg-white hover:text-blue-900 rounded-md py-1 border-white'>rulers</div>
-            </div>
-        </div>
-    )
+        
+            )
 }
 
-
-// setScore(() => {
-//     if (win === "you win") {
-//          setScore(score+1);
-//     }
-//     else{
-//         return score
-//     }
-// })
+ 
